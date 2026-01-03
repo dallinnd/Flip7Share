f343bd90852cc4d571b42c24c9d48cbd4dd05273
dallinnd <156145316+dallinnd@users.noreply.github.com>
2026-01-02 23:23:37 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,27 @@
-const CACHE_NAME = 'f7-v1';
-const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
+const CACHE_NAME = 'f7-v2';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
 
 self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(c => {
+            console.log('Caching all assets');
+            return c.addAll(ASSETS);
+        })
+    );
 });
 
 self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+    e.respondWith(
+        caches.match(e.request).then(res => {
+            return res || fetch(e.request);
+        })
+    );
 });
